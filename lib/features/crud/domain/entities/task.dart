d23f5e804e9c2366b50bbcd15625b5f73c61b6cc
Luciano <luciano01.dev@gmail.com>
2021-12-21 14:24:31 -0300

    Implement domain/entity.

---
+++ lib/features/crud/domain/entities/task.dart
@@ -0,0 +1,15 @@
+class Task {
+  String? uid;
+  String? name;
+  bool? isCompleted;
+  String? date;
+  String? time;
+
+  Task({
+    required this.uid,
+    required this.name,
+    required this.isCompleted,
+    required this.date,
+    required this.time,
+  });
+}
